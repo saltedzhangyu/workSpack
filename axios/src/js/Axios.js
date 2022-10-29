86f51816d8d9ff8861bee7e4cbd02aa7ced2b2de
zhangyu <finalzhangyu@gmail.com>
2022-10-29 14:04:40 +0800

    axios 1.0.1 源码 模拟实现axios发送请求

--- axios/src/js/Axios.js
+++ axios/src/js/Axios.js
@@ -9,12 +9,46 @@ function Axios(config) {
 }
 
 Axios.prototype.request = function (config) {
-    console.log("发送ajax请求 === request " + config)
+    let promise = Promise.resolve(config)
+    let chains = [dispatchRequest, undefined]
+    return promise.then(chains[0], chains[1])
 }
 
 Axios.prototype.get = function (config) {
     console.log("将通过get发送ajax请求")
     return this.request(config)
+}
+
+function dispatchRequest(config) {
+    return xhrAdapter(config).then(value => {
+        return value
+    }, reason => {
+        return reason
+    })
+}
+
+function xhrAdapter(config) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(config.method, config.url)
+        xhr.send();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve({
+                        config,
+                        data: xhr.response,
+                        headers: xhr.getAllResponseHeaders(),
+                        request: xhr,
+                        status: xhr.status,
+                        statusText: xhr.statusText
+                    })
+                } else {
+                    reject(new Error("请求失败" + xhr.status))
+                }
+            }
+        }
+    })
 }
 
 function createInstance(config) {
@@ -28,8 +62,13 @@ function createInstance(config) {
     })
     return instance
 }
+
 let axios = createInstance({method: "Get"})
-axios.get()
-axios.request()
-axios()
-console.dir(axios)
+
+
+axios.get({
+    url:"http://localhost:3000/posts",
+    method: "get"
+}).then(value => {
+    console.log(value)
+})
